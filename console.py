be810ce6062f8d2b685badf648d0fc2f564cd19e
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-11-22 20:36:07 +0100

    DBStorage - States and Cities

--- console.py
+++ console.py
@@ -234,11 +234,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in models.storage._FileStorage__objects.items():
+            for k, v in models.storage.items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in models.storage._FileStorage__objects.items():
+            for k, v in models.storage.items():
                 print_list.append(str(v))
 
         print(print_list)
@@ -251,7 +251,7 @@ class HBNBCommand(cmd.Cmd):
     def do_count(self, args):
         """Count current number of class instances"""
         count = 0
-        for k, v in models.storage._FileStorage__objects.items():
+        for k, v in models.storage.items():
             if args == k.split('.')[0]:
                 count += 1
         print(count)
