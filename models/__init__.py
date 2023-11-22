4f8eaf7e5ba5893b0d12d8a4a34dd96cbd25857c
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-11-22 22:28:02 +0100

    DBStorage - Amenities

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,9 @@
 
 from os import getenv
 
+# List of models to be imported when using wildcard import
+__all__ = ["BaseModel", "User", "State", "City", "Amenity", "Place", "Review"]
+
 storage_table = getenv("HBNB_TYPE_STORAGE")
 
 if storage_table == "db":
@@ -11,4 +14,14 @@ if storage_table == "db":
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+
+# Import model classes
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 storage.reload()
